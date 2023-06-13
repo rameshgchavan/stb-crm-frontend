086e08cd8a9b917bda9357f16ab9b7096ad0d881
Ramesh_Chavan <ramesh7452@gmail.com>
2023-06-13 19:23:42 +0530

    empty array and conditions added

--- src/components/forms/packageForm/CustomerSection.jsx
+++ src/components/forms/packageForm/CustomerSection.jsx
@@ -10,20 +10,20 @@ const CustomerSection = ({ acNo }) => {
     const {
         CustName: name, Area: area, Address: address, MobNo: mobile,
         LCOCode: lcoCode, VC_NDS_MAC_ID: vc_Nds_Mac_ID
-    } = customer;
+    } = customer || [];
 
     return (
         <FormGroup className="border shadow rounded p-3 mb-3">
-            <Form.Label className="d-block fs-5 text-uppercase text-primary fw-bold">{name}</Form.Label>
+            <Form.Label className="d-block fs-5 text-uppercase text-primary fw-bold">{name ? name : "N/A"}</Form.Label>
 
-            <Form.Label className="d-block fw-bold">Area: {area}, {address}</Form.Label>
+            <Form.Label className="d-block fw-bold">Area: {area ? area : "N/A"}, {address}</Form.Label>
 
-            <Form.Label className="d-block fw-bold">Mobile: {mobile}</Form.Label>
+            <Form.Label className="d-block fw-bold">Mobile: {mobile ? mobile : "N/A"}</Form.Label>
             <hr />
 
             <div className="d-sm-flex justify-content-around">
-                <Form.Label className="d-block fw-bold">LCO: {lcoCode}</Form.Label>
-                <Form.Label className="d-block fw-bold">ID: {vc_Nds_Mac_ID}</Form.Label>
+                <Form.Label className="d-block fw-bold">LCO: {lcoCode ? lcoCode : "N/A"}</Form.Label>
+                <Form.Label className="d-block fw-bold">ID: {vc_Nds_Mac_ID ? vc_Nds_Mac_ID : "N/A"}</Form.Label>
                 <Form.Label className="d-block fw-bold">A/c No: {acNo}</Form.Label>
             </div>
         </FormGroup>
