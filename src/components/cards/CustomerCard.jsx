aa1ebdd76d2620f61fd0940fb3318556c9628718
Ramesh_Chavan <ramesh7452@gmail.com>
2023-06-13 19:17:34 +0530

    imported react icons

--- src/components/cards/CustomerCard.jsx
+++ src/components/cards/CustomerCard.jsx
@@ -1,6 +1,13 @@
-import { Button, Form } from "react-bootstrap"
+import { Form } from "react-bootstrap"
 import { useNavigate } from "react-router-dom";
 import { DateTime } from "luxon";
+
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { RiCheckboxMultipleLine } from "react-icons/ri"
+import { FaWhatsappSquare } from "react-icons/fa"
+import { MdReadMore } from "react-icons/md"
+import { SiBookstack } from "react-icons/si"
+
 
 const CustomerCard = ({ customer, srNo }) => {
     const navigate = useNavigate();
@@ -22,31 +29,53 @@ const CustomerCard = ({ customer, srNo }) => {
         AreaPerson: areaPerson,
     } = customer;
 
+    const copyToShare = `*Name:* ${name}
+*Area:* ${area}
+*Mobile:* ${mobile}
+*Area||Person:* ${areaPerson}
+*A/c:* ${acNo}
+*LCO:* ${lcoCode}
+*ID:* ${vcNdsMacId}`;
+
     return (
         <Form >
             <Form.Group className="border rounded shadow px-3 py-2 mb-3" style={{ width: "20rem" }}>
                 <Form.Label className="d-flex justify-content-between fw-bold">
                     <div className="fs-6 fw-bold text-start">{srNo}.</div>
-                    <div className="fs-6 fw-bold text-start">A/c:{acNo}</div>
-                    {status == "ACTIVE" ? <div className="fs-6 fw-bold text-lowercase text-success text-start">{status}</div>
-                        : <div className="fs-6 fw-bold text-lowercase text-danger text-start">{status}</div>}
+                    <div style={{ fontSize: "0.75rem" }}
+                        className={`${status == "ACTIVE" ? "text-success" : "text-danger"}
+                     text-uppercase fw-bolder text-start`}>{status}</div>
+                      <div className="fs-6 fw-bold text-start">Ac:{acNo}</div>
                 </Form.Label>
 
                 <Form.Label className="d-block text-uppercase fw-bold text-primary text-truncate">{name}</Form.Label>
                 <Form.Label className="d-block text-truncate">{area}, {address}.<br /> Mobile: {mobile}</Form.Label>
 
                 <Form.Label className="d-flex fw-bold justify-content-between">
-                    <div className="fs-6 fw-bold">LCO:{lcoCode}</div>
-                    <div className="fs-6 fw-bold text-primary">ID:{vcNdsMacId}</div>
+                    <div className="fw-bold">LCO:{lcoCode}</div>
+                    <div className="fw-bold text-primary">ID:{vcNdsMacId}</div>
+
+                    <CopyToClipboard text={vcNdsMacId}>
+                        <RiCheckboxMultipleLine size={18} className="text-secondary"
+                            onClick={() => alert("ID copied.")}
+                        />
+                    </CopyToClipboard>
                 </Form.Label>
 
                 <div className="d-flex justify-content-between">
-                    <Button variant="warning" size="sm"
+                    <SiBookstack size={30} className="text-warning"
                         onClick={() => { navigate(`/package/${acNo}/${DateTime.now().minus({ months: 1 }).toFormat("LLL-yyyy")}`) }}
-                    >Pack</Button>
-                    <Button size="sm"
+                    />
+
+                    <CopyToClipboard text={copyToShare}>
+                        <FaWhatsappSquare size={30} className="text-success"
+                            onClick={() => alert("Copied to share on whatsapp")}
+                        />
+                    </CopyToClipboard>
+
+                    <MdReadMore size={35} className="text-primary"
                         onClick={() => { navigate(`/customer/${id}`) }}
-                    >more...</Button>
+                    />
                 </div>
 
                 <hr />
