8b86db3e45eec7de01456e31091b1e67b9983400
Ramesh_Chavan <ramesh7452@gmail.com>
2023-06-16 00:13:32 +0530

    emailjs code uncommented

--- src/components/security/Signup.jsx
+++ src/components/security/Signup.jsx
@@ -48,14 +48,14 @@ const Signup = () => {
             alert(`${emailID.current} is already in use, try another one.`)
         }
         else if ((isAdmin && isUserEmail.data.code == 404) || (!isAdmin && isUserEmail.data.code == 404 && isAdminEmail.data.code == 200)) {
-            console.warn(emailOTP);
+            // console.warn(emailOTP);
 
-            // emailjs.sendForm('service_6bhhezj', 'template_svo2tbq', otpForm.current, 'llSBBJFE7skawlOYO')
-            //     .then((result) => {
-            //         console.warn(result.text);
-            //     }, (error) => {
-            //         console.warn(error.text);
-            //     });
+            emailjs.sendForm('service_6bhhezj', 'template_svo2tbq', otpForm.current, 'llSBBJFE7skawlOYO')
+                .then((result) => {
+                    console.warn(result.text);
+                }, (error) => {
+                    console.warn(error.text);
+                });
 
             setDidsabled(true);
             setHidden(false);
