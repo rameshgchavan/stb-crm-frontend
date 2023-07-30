98ce01b77b198331c1707ee81edd4d49a4935689
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-30 11:12:54 +0530

    Reset setting added of local storage

--- src/components/security/Login.jsx
+++ src/components/security/Login.jsx
@@ -53,9 +53,22 @@ const Login = () => {
             return
         }
 
+        const filterSetting = JSON.parse(localStorage.getItem("FilterSetting"));
+
+        const resetSetting = {
+            ...filterSetting,
+            custAreaPerson: "All",
+            custAreaManager: "All",
+            custLocation: "INLINE",
+            transAreaPerson: "All",
+            transAreaManager: "All",
+            transDay: "All",
+        }
+
+        localStorage.setItem("FilterSetting", JSON.stringify(resetSetting));
+
         dispatch(authenticateUserAction(user.data))
         navigate("/customers")
-
     }
 
     return (
@@ -87,6 +100,19 @@ const Login = () => {
             <Nav className='d-flex flex-column align-items-center mt-4'>
                 <Nav.Link onClick={() => navigate("/forgotpass")}>Forgot password</Nav.Link>
             </Nav>
+
+            <hr />
+            <div>
+                Use following IDs to test this application<br />
+
+                <b>Admin ID:</b><br />
+                demoadmin@gmail.com<br />
+                <b> Admin's User IDs:</b> <br />
+                demouser1@gmail.com<br />
+                demouser2@gmail.com<br />
+                <br />
+                <b> All IDs password:</b> Demo@123
+            </div>
         </Container>
     )
 }
