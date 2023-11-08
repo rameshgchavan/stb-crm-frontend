ce654e4d9de36059c6621bcf55333b9d41e8bd82
Ramesh_Chavan <ramesh7452@gmail.com>
2023-11-09 03:18:11 +0530

    navigate removed and modals added

--- src/components/cards/CustomerCard.jsx
+++ src/components/cards/CustomerCard.jsx
@@ -1,5 +1,4 @@
 import { Form } from "react-bootstrap"
-import { useNavigate } from "react-router-dom";
 import { DateTime } from "luxon";
 
 import { CopyToClipboard } from "react-copy-to-clipboard";
@@ -7,10 +6,14 @@ import { RiCheckboxMultipleLine } from "react-icons/ri"
 import { FaWhatsappSquare } from "react-icons/fa"
 import { MdReadMore } from "react-icons/md"
 import { SiBookstack } from "react-icons/si"
+import CustomerModal from "../modals/CustomerModal";
+import { useState } from "react";
+import PackageModal from "../modals/PackageModal";
 
 
 const CustomerCard = ({ customer, srNo }) => {
-    const navigate = useNavigate();
+    const [customerModalShow, setCustomerModalShow] = useState(false);
+    const [packageModalShow, setPackageModalShow] = useState(false);
 
     // Destructured and put aliases. Here srNo is an alias of SrNo
     // Its not an object of key value pair.
@@ -38,68 +41,85 @@ const CustomerCard = ({ customer, srNo }) => {
 *ID:* ${vcNdsMacId}`;
 
     return (
-        <Form >
-            <Form.Group className="border rounded shadow px-3 py-2 mb-3" style={{ width: "20rem" }}>
-                <Form.Label className="d-flex justify-content-between fw-bold">
-                    <div className="fs-6 fw-bold text-start">{srNo}.</div>
-                    <div style={{ fontSize: "0.75rem" }}
-                        className={`${status == "ACTIVE" ? "text-success" : "text-danger"}
+        <>
+            <Form >
+                <Form.Group className="border rounded shadow px-3 py-2 mb-3" style={{ width: "20rem" }}>
+                    <Form.Label className="d-flex justify-content-between fw-bold">
+                        <div className="fs-6 fw-bold text-start">{srNo}.</div>
+                        <div style={{ fontSize: "0.75rem" }}
+                            className={`${status == "ACTIVE" ? "text-success" : "text-danger"}
                      text-uppercase fw-bolder text-start`}>{status}</div>
-                      <div className="fs-6 fw-bold text-start">Ac:{acNo}</div>
-                </Form.Label>
+                        <div className="fs-6 fw-bold text-start">Ac:{acNo}</div>
+                    </Form.Label>
 
-                <Form.Label className="d-block text-uppercase fw-bold text-primary text-truncate">{name}</Form.Label>
-                <Form.Label className="d-block text-truncate">{area}, {address}.<br /> Mobile: {mobile}</Form.Label>
+                    <Form.Label className="d-block text-uppercase fw-bold text-primary text-truncate">{name}</Form.Label>
+                    <Form.Label className="d-block text-truncate">{area}, {address}.<br /> Mobile: {mobile}</Form.Label>
 
-                <Form.Label className="d-flex fw-bold justify-content-between">
-                    <div className="fw-bold">LCO:{lcoCode}</div>
-                    <div className="fw-bold text-primary">ID:{vcNdsMacId}</div>
+                    <Form.Label className="d-flex fw-bold justify-content-between">
+                        <div className="fw-bold">LCO:{lcoCode}</div>
+                        <div className="fw-bold text-primary">ID:{vcNdsMacId}</div>
 
-                    <CopyToClipboard text={vcNdsMacId}>
-                        <RiCheckboxMultipleLine size={18} className="text-secondary"
-                            onClick={() => alert("ID copied.")}
+                        <CopyToClipboard text={vcNdsMacId}>
+                            <RiCheckboxMultipleLine size={18} className="text-secondary"
+                                onClick={() => alert("ID copied.")}
+                            />
+                        </CopyToClipboard>
+                    </Form.Label>
+
+                    <div className="d-flex justify-content-between">
+                        <SiBookstack size={30} className="text-warning"
+                            onClick={() => setPackageModalShow(true)}
                         />
-                    </CopyToClipboard>
-                </Form.Label>
 
-                <div className="d-flex justify-content-between">
-                    <SiBookstack size={30} className="text-warning"
-                        onClick={() => { navigate(`/package/${acNo}/${DateTime.now().minus({ months: 1 }).toFormat("LLL-yyyy")}`) }}
-                    />
+                        <CopyToClipboard text={copyToShare}>
+                            <FaWhatsappSquare size={30} className="text-success"
+                                onClick={() => alert("Copied to share on whatsapp")}
+                            />
+                        </CopyToClipboard>
 
-                    <CopyToClipboard text={copyToShare}>
-                        <FaWhatsappSquare size={30} className="text-success"
-                            onClick={() => alert("Copied to share on whatsapp")}
+                        <MdReadMore size={35} className="text-primary"
+                            onClick={() => setCustomerModalShow(true)}
                         />
-                    </CopyToClipboard>
-
-                    <MdReadMore size={35} className="text-primary"
-                        onClick={() => { navigate(`/customer/${id}`) }}
-                    />
-                </div>
-
-                <hr />
-                <Form.Label className="d-flex justify-content-between text-truncate" >
-                    <div className="me-3 lh-1">
-                        <span style={{ fontSize: "x-small" }}>
-                            {areaManager ? "Manager" : "NA"}
-                        </span><br />
-                        <span style={{ fontSize: "smaller", fontWeight: "bold" }}>
-                            {areaManager ? areaManager : "NA"}
-                        </span>
                     </div>
 
-                    <div className="text-truncate lh-1">
-                        <span style={{ fontSize: "x-small" }}>
-                            {areaPerson != areaManager && "Area"}
-                        </span><br />
-                        <span style={{ fontSize: "smaller", fontWeight: "bold" }}>
-                            {areaPerson != areaManager && areaPerson}
-                        </span>
-                    </div>
-                </Form.Label>
-            </Form.Group>
-        </Form>
+                    <hr />
+                    <Form.Label className="d-flex justify-content-between text-truncate" >
+                        <div className="me-3 lh-1">
+                            <span style={{ fontSize: "x-small" }}>
+                                {areaManager ? "Manager" : "NA"}
+                            </span><br />
+                            <span style={{ fontSize: "smaller", fontWeight: "bold" }}>
+                                {areaManager ? areaManager : "NA"}
+                            </span>
+                        </div>
+
+                        <div className="text-truncate lh-1">
+                            <span style={{ fontSize: "x-small" }}>
+                                {areaPerson != areaManager && "Area"}
+                            </span><br />
+                            <span style={{ fontSize: "smaller", fontWeight: "bold" }}>
+                                {areaPerson != areaManager && areaPerson}
+                            </span>
+                        </div>
+                    </Form.Label>
+                </Form.Group>
+            </Form>
+
+            <CustomerModal
+                showMe={customerModalShow}
+                closeMe={setCustomerModalShow}
+                title={"Edit customer"}
+                id={id}
+            />
+
+            <PackageModal
+                showMe={packageModalShow}
+                closeMe={setPackageModalShow}
+                title={"Package"}
+                acNo={acNo}
+                transactionDate={DateTime.now().minus({ months: 1 }).toFormat("LLL-yyyy")}
+            />
+        </>
     )
 }
 
--- src/components/cards/TransactionCard.jsx
+++ src/components/cards/TransactionCard.jsx
@@ -1,14 +1,17 @@
 import { Button, Form, FormGroup } from "react-bootstrap"
-import { useNavigate } from "react-router-dom";
 import { DateTime } from "luxon";
 
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { FaWhatsappSquare } from "react-icons/fa"
 import { MdReadMore } from "react-icons/md"
 import { RiCheckboxMultipleLine } from "react-icons/ri"
+import { useState } from "react";
+import CustomerModal from "../modals/CustomerModal";
+import PackageModal from "../modals/PackageModal";
 
 const TransactionCard = ({ transaction, srNo }) => {
-    const navigate = useNavigate();
+    const [customerModalShow, setCustomerModalShow] = useState(false);
+    const [packageModalShow, setPackageModalShow] = useState(false);
 
     // Destructured and put aliases. Here rechargeDate is an alias of TransactionDateTime
     // Its not an object of key value pair.
@@ -37,67 +40,84 @@ const TransactionCard = ({ transaction, srNo }) => {
 *ID:* ${vcNdsMacId}`;
 
     return (
-        <Form >
-            <FormGroup className="border rounded shadow px-3 py-2 mb-3" style={{ width: "20rem" }}>
-                <Form.Label className="d-flex justify-content-between fw-bold">
-                    <div className="text-start">{srNo}.</div>
-                    <div style={{ fontSize: "0.8rem" }} className="text-success text-nowrap">{DateTime.fromISO(rechargeDate).toFormat("dd-LLL-yy")}</div>
-                    <div style={{ fontSize: "0.8rem" }} className="text-danger text-nowrap">{DateTime.fromISO(lastDate).toFormat("dd-LLL-yy")}</div>
-                    <div>Ac:{acNo}</div>
-                </Form.Label>
+        <>
+            <Form >
+                <FormGroup className="border rounded shadow px-3 py-2 mb-3" style={{ width: "20rem" }}>
+                    <Form.Label className="d-flex justify-content-between fw-bold">
+                        <div className="text-start">{srNo}.</div>
+                        <div style={{ fontSize: "0.8rem" }} className="text-success text-nowrap">{DateTime.fromISO(rechargeDate).toFormat("dd-LLL-yy")}</div>
+                        <div style={{ fontSize: "0.8rem" }} className="text-danger text-nowrap">{DateTime.fromISO(lastDate).toFormat("dd-LLL-yy")}</div>
+                        <div>Ac:{acNo}</div>
+                    </Form.Label>
 
-                <Form.Label className="d-block text-uppercase fw-bold text-primary text-truncate">{name ? name : "N/A"}</Form.Label>
-                <Form.Label className="d-block text-truncate">{area}<br /> {mobile ? mobile : "N/A"}</Form.Label>
+                    <Form.Label className="d-block text-uppercase fw-bold text-primary text-truncate">{name ? name : "N/A"}</Form.Label>
+                    <Form.Label className="d-block text-truncate">{area}<br /> {mobile ? mobile : "N/A"}</Form.Label>
 
-                <Form.Label className="d-flex fw-bold justify-content-between">
-                    <div>LCO:{lcoCode ? lcoCode : "N/A"}</div>
-                    <div className="text-primary">ID:{vcNdsMacId ? vcNdsMacId : "N/A"}</div>
+                    <Form.Label className="d-flex fw-bold justify-content-between">
+                        <div>LCO:{lcoCode ? lcoCode : "N/A"}</div>
+                        <div className="text-primary">ID:{vcNdsMacId ? vcNdsMacId : "N/A"}</div>
 
-                    <CopyToClipboard text={vcNdsMacId}>
-                        <RiCheckboxMultipleLine size={18} className="text-secondary"
-                            onClick={() => alert("ID copied.")}
+                        <CopyToClipboard text={vcNdsMacId}>
+                            <RiCheckboxMultipleLine size={18} className="text-secondary"
+                                onClick={() => alert("ID copied.")}
+                            />
+                        </CopyToClipboard>
+                    </Form.Label>
+
+                    <div className="d-flex justify-content-between">
+                        <Button variant="warning"
+                            onClick={() => setPackageModalShow(true)}
+                        >₹{bill}</Button>
+
+                        <CopyToClipboard text={copyToShare}>
+                            <FaWhatsappSquare size={30} className="text-success"
+                                onClick={() => alert("Copied to share on whatsapp")}
+                            />
+                        </CopyToClipboard>
+
+                        <MdReadMore size={35} className="text-primary"
+                            onClick={() => setCustomerModalShow(true)}
                         />
-                    </CopyToClipboard>
-                </Form.Label>
-
-                <div className="d-flex justify-content-between">
-                    <Button variant="warning"
-                        onClick={() => { navigate(`/package/${acNo}/${DateTime.fromISO(rechargeDate).toISODate()}`) }}
-                    >₹{bill}</Button>
-
-                    <CopyToClipboard text={copyToShare}>
-                        <FaWhatsappSquare size={30} className="text-success"
-                            onClick={() => alert("Copied to share on whatsapp")}
-                        />
-                    </CopyToClipboard>
-
-                    <MdReadMore size={35} className="text-primary"
-                        onClick={() => { navigate(`/customer/${id}`) }}
-                    />
-                </div>
-
-                <hr />
-                <Form.Label className="d-flex justify-content-between text-truncate" >
-                    <div className="me-3 lh-1">
-                        <span style={{ fontSize: "x-small" }}>
-                            {areaManager ? "Manager" : "N/A"}
-                        </span><br />
-                        <span style={{ fontSize: "smaller", fontWeight: "bold" }}>
-                            {areaManager ? areaManager : "N/A"}
-                        </span>
                     </div>
 
-                    <div className="text-truncate lh-1">
-                        <span style={{ fontSize: "x-small" }}>
-                            {areaPerson != areaManager && "Area"}
-                        </span><br />
-                        <span style={{ fontSize: "smaller", fontWeight: "bold" }}>
-                            {areaPerson != areaManager && areaPerson}
-                        </span>
-                    </div>
-                </Form.Label>
-            </FormGroup>
-        </Form>
+                    <hr />
+                    <Form.Label className="d-flex justify-content-between text-truncate" >
+                        <div className="me-3 lh-1">
+                            <span style={{ fontSize: "x-small" }}>
+                                {areaManager ? "Manager" : "N/A"}
+                            </span><br />
+                            <span style={{ fontSize: "smaller", fontWeight: "bold" }}>
+                                {areaManager ? areaManager : "N/A"}
+                            </span>
+                        </div>
+
+                        <div className="text-truncate lh-1">
+                            <span style={{ fontSize: "x-small" }}>
+                                {areaPerson != areaManager && "Area"}
+                            </span><br />
+                            <span style={{ fontSize: "smaller", fontWeight: "bold" }}>
+                                {areaPerson != areaManager && areaPerson}
+                            </span>
+                        </div>
+                    </Form.Label>
+                </FormGroup>
+            </Form>
+
+            <CustomerModal
+                showMe={customerModalShow}
+                closeMe={setCustomerModalShow}
+                title={"Edit customer"}
+                id={id}
+            />
+
+            <PackageModal
+                showMe={packageModalShow}
+                closeMe={setPackageModalShow}
+                title={"Package"}
+                acNo={acNo}
+                transactionDate={DateTime.fromISO(rechargeDate).toISODate()}
+            />
+        </>
     )
 }
 
