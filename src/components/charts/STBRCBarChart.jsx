9a0bcb7308a71197bbf3da462d48a21600740edf
Ramesh_Chavan <ramesh7452@gmail.com>
2023-11-19 03:36:16 +0530

    unwanted code removed and stbs recharges devided into pain stbs and free stbs

--- src/components/charts/STBRCBarChart.jsx
+++ src/components/charts/STBRCBarChart.jsx
@@ -6,9 +6,10 @@ import checkAdminGetDbName from "../../functions/checkAdminGetDbName";
 import { Form } from "react-bootstrap";
 import { DateTime } from "luxon";
 
-const STBRCBarChart = (props) => {
+const STBRCBarChart = () => {
+    const customersList = useSelector(state => state.customersListReducer)?.data;
     const scrutinizedUser = useSelector(state => state.scrutinyUserReducer); // to get token
-    const { isAdmin, dbName } = checkAdminGetDbName(scrutinizedUser);
+    const { dbName } = checkAdminGetDbName(scrutinizedUser);
 
     const monthsList = [
         "Jan", "Feb", "Mar", "Apr",
@@ -22,17 +23,23 @@ const STBRCBarChart = (props) => {
         yearsList.push(year);
     }
 
-    // const { monthsList, yearsList } = props;
-
     const yearOptions = useRef(DateTime.now().toFormat("yyyy"));
 
     const [stbData, setSTBData] = useState({
-        labels: [],
-        datasets: [{
-            label: "Recharged STBs",
-            data: [],
-            backgroundColor: ["green"]
-        }]
+        paidSTBs: {
+            labels: [],
+            datasets: [{
+                label: "Paid STBs",
+                data: []
+            }]
+        },
+        freeSTBs: {
+            labels: [],
+            datasets: [{
+                label: "Free STBs",
+                data: []
+            }]
+        }
     });
 
     useEffect(() => {
@@ -40,7 +47,7 @@ const STBRCBarChart = (props) => {
     }, [])
 
     const listTransactions = async () => {
-        const dataList = (await axios(`/transactions/rcstbcount`,
+        const acNoData = (await axios(`/transactions/rcstbcount`,
             {
                 method: "post",
                 headers: { authorization: `bearer ${scrutinizedUser.token}` },
@@ -48,32 +55,76 @@ const STBRCBarChart = (props) => {
             }
         ))?.data
 
+        const freeSTBData = await acNoData.map((acNoList) => {
+            return acNoList.map((acNos) => {
+                //Populated customer for transactions 
+                let customerIndex = customersList.findIndex((customer) => customer.AcNo === acNos.AcNo)
+
+                return customerIndex === -1
+                    ? {
+                        AcNo: acNos.AcNo,
+                        IsFree: false
+                    }
+                    : {
+                        AcNo: customersList[customerIndex].AcNo,
+                        IsFree: customersList[customerIndex].IsFree || false
+                    }
+
+            }).filter((sorted) => sorted.IsFree === true);
+        });
+
         setSTBData({
-            labels: monthsList,
-            datasets: [{
-                label: "Recharged STBs",
-                data: dataList,
-                backgroundColor: ["green"]
-            }]
+            paidSTBs: {
+                labels: monthsList,
+                datasets: [
+                    {
+                        label: "Paid STBs",
+                        data: acNoData.map((acNos, index) => { return acNos.length - freeSTBData[index].length }),
+                        backgroundColor: ["blue"]
+                    }
+                ]
+            },
+
+            freeSTBs: {
+                labels: monthsList,
+                datasets: [
+                    {
+                        label: "Free STBs",
+                        data: freeSTBData.map((freeSTBs) => { return freeSTBs.length }),
+                        backgroundColor: ["Orange"]
+                    }
+                ]
+            }
         });
     }
 
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+    }
 
     return (
-        <div style={{ position: "relative", height: "50vh", width: "80vw", margin: "auto" }}>
-            <Form.Select name="year" defaultValue={yearOptions.current} style={{ width: "100px" }}
-                onChange={(e) => {
-                    yearOptions.current = e.target.value;
-                    listTransactions()
-                }}
-            >
-                {yearsList?.map((year, index) =>
-                    <option key={index} value={year}>{year}</option>
-                )}
-            </Form.Select>
+        <>
+            <div style={{ position: "relative", width: "80vw", margin: "auto" }}>
+                <Form.Select name="year" defaultValue={yearOptions.current} style={{ width: "100px" }}
+                    onChange={(e) => {
+                        yearOptions.current = e.target.value;
+                        listTransactions()
+                    }}
+                >
+                    {yearsList?.map((year, index) =>
+                        <option key={index} value={year}>{year}</option>
+                    )}
+                </Form.Select>
+            </div>
 
-            <Bar data={stbData} options={{ responsive: true, maintainAspectRatio: false }} />
-        </div>
+            <div style={{ position: "relative", height: "50vh", width: "80vw", margin: "auto" }} >
+                <Bar data={stbData.paidSTBs} options={options} />
+            </div >
+            <div style={{ position: "relative", height: "20vh", width: "80vw", margin: "auto" }} >
+                <Bar data={stbData.freeSTBs} options={options} />
+            </div >
+        </>
     )
 }
 
