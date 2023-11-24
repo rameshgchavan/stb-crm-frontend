8a33093a14c9a298fc9ee809455c2f426324a936
Ramesh_Chavan <ramesh7452@gmail.com>
2023-11-24 16:49:29 +0530

    Routes updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Header from './components/Header';
-import FilterRoutes from './routes/FilterRoutes';
-import AllRoutes from './routes';
+import filterRoutes from './routes/filterRoutes';
+import pageRoutes from './routes/pageRoutes';
+import { useSelector } from 'react-redux';
 
 function App() {
+  // Get scrutinized user from redux store
+  const scrutinizedUser = useSelector(state => state.scrutinyUserReducer);
+
   return (
     <div className="App">
-      <Header filter={<FilterRoutes />} />
+      {/* Passed routed filer component to Header componet */}
+      <Header filter={filterRoutes(scrutinizedUser)} />
 
-      <AllRoutes />
+      {/* Here rendering routed page component*/}
+      <div className='mt-3'>
+        {pageRoutes(scrutinizedUser)}
+      </div>
     </div>
   );
 }
