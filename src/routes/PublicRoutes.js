bbeadbdf5691e30ab8687e27adba3b68f01d7b07
Ramesh_Chavan <ramesh7452@gmail.com>
2024-01-11 12:14:05 +0530

    page not found  page removed, home page added, / removed from path and ( ) removed

--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -1,19 +1,19 @@
-import { Route } from 'react-router-dom';
+import { Outlet, Route } from 'react-router-dom';
 
+// Pages
+import HomePage from '../pages/HomePage';
 import Signup from "../components/security/Signup";
 import ForgotPassword from "../components/security/ForgotPassword";
 import Login from '../components/security/Login';
-import PageNotFound from "../pages/PageNotFound";
 
 // This routes used by pageRoutes/index.js
-// Initialized public routes and exported
-const publicRoutes = (
-    <Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgotpass" element={<ForgotPassword />} />
-        <Route path="*" element={<PageNotFound />} />
+// Initialized public page routes and exported
+const publicRoutes =
+    <Route path="/" element={<Outlet />}>
+        <Route index element={<HomePage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="forgotpass" element={<ForgotPassword />} />
     </Route>
-)
 
-export default publicRoutes
+export default publicRoutes;
