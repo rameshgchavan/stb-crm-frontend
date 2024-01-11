cec562a34c44a3a2269feded525b562465692589
Ramesh_Chavan <ramesh7452@gmail.com>
2024-01-11 12:09:28 +0530

    Home page added

---
+++ src/pages/HomePage.js
@@ -0,0 +1,28 @@
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <div>
+                Following IDs are to test this application<br />
+
+                <b>Admin ID:</b><br />
+                demoadmin@gmail.com<br />
+                <b>Admin's User IDs:</b> <br />
+                demouser1@gmail.com<br />
+                demouser2@gmail.com<br />
+                <br />
+                <b> All IDs password:</b> Demo@123
+            </div>
+            <hr />
+            <Button
+                onClick={() => navigate("/login")}
+            >Login</Button>
+        </div>
+    )
+}
+
+export default HomePage
