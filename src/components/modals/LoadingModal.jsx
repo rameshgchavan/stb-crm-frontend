a716848788398fde48d4f8ce0e82360d74c0e651
Ramesh_Chavan <ramesh7452@gmail.com>
2024-02-02 05:13:38 +0530

    loading modal updated

--- src/components/modals/LoadingModal.jsx
+++ src/components/modals/LoadingModal.jsx
@@ -4,25 +4,23 @@ import { useSelector } from "react-redux";
 // This component used by pages/LoadingsPage, cards/LoadingCard and cards/TransactionCard
 const LoadingModal = () => {
   const { isLoading: showMe } = useSelector(state => state.loadingReducer);
-  
+
   if (!showMe) return null;
 
   return (
-    <div >
-      <Modal show={showMe} fullscreen={true}>
-        {/* <Modal.Header closeButton>
-          <Modal.Title style={{ height: "16px", fontSize: "16px" }}>Loading</Modal.Title>
-        </Modal.Header> */}
-        <Modal.Body>
-          <h3>Loading... please wait</h3>
-        </Modal.Body>
-        {/* <Modal.Footer>
+    <Modal show={showMe} className="d-flex align-items-center">
+      <Modal.Header >
+          <Modal.Title style={{ height: "16px", fontSize: "16px" }}>Please wait</Modal.Title>
+        </Modal.Header>
+      <Modal.Body className="text-center">
+        <h4>Loading...</h4>
+      </Modal.Body>
+      {/* <Modal.Footer>
                     <Button variant="secondary" onClick={() => closeMe(false)}>
                         Close
                     </Button>
                 </Modal.Footer> */}
-      </Modal>
-    </div>
+    </Modal>
   )
 }
 
