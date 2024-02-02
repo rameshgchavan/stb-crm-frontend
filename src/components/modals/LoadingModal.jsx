b97b26a5535d070163e25a86ed38e71a6944f5ac
Ramesh_Chavan <ramesh7452@gmail.com>
2024-02-02 19:25:34 +0530

    loading modal updated with gif

--- src/components/modals/LoadingModal.jsx
+++ src/components/modals/LoadingModal.jsx
@@ -8,12 +8,14 @@ const LoadingModal = () => {
   if (!showMe) return null;
 
   return (
-    <Modal show={showMe} className="d-flex align-items-center">
+    <Modal show={showMe} className="d-flex justify-content-center align-items-center">
       <Modal.Header >
-          <Modal.Title style={{ height: "16px", fontSize: "16px" }}>Please wait</Modal.Title>
-        </Modal.Header>
+        <Modal.Title style={{ height: "16px", fontSize: "16px" }}>Please wait</Modal.Title>
+      </Modal.Header>
       <Modal.Body className="text-center">
-        <h4>Loading...</h4>
+        <h4>Loading<img src="/loading.gif" alt="working" width="100" height="20"/></h4>
+       
+        <img src="/working.gif" alt="working" width="200" height="135"/>
       </Modal.Body>
       {/* <Modal.Footer>
                     <Button variant="secondary" onClick={() => closeMe(false)}>
